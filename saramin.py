927d8e9b529767ff7875db009609b0f785b24da0
minyeamer <kimmy9809@gmail.com>
2022-04-07 23:42:07 +0900

    2022-05-07 update saramin api request process

--- saramin.py
+++ saramin.py
@@ -1,42 +1,116 @@
+import requests
+import json
+from datetime import date
 from content import Content
 from admin_info import get_saramin_key
 
+
 class Saramin(Content):
     def __init__(self, requires: dict):
-        super().__init___(requires)
+        super().__init__(requires)
         self.service_key = get_saramin_key()
         self.service_url = 'https://oapi.saramin.co.kr/job-search'
+        self.service_response = None
+
 
     def get_content(self) -> dict:
         self.set_headers()
         self.set_params()
+        self.request_json()
+        self.make_content()
 
         return self.content
 
+
     def set_headers(self):
         self.headers['Accept'] = 'application/json'
-    
+
+
     def set_params(self):
         requires = self.requires
         locations = requires['locations']
         jobs = requires['jobs']
-        loc_dict = requires['code_map']['saramin_loc']
-        job_dict = requires['code_map']['saramin_job']
+        # loc_dict = requires['code_map']['saramin_loc']
+        # job_dict = requires['code_map']['saramin_job']
 
-        loc_list = self.map_locations(locations, loc_dict)
-        job_list = self.map_jobs(jobs, job_dict)
+        # loc_list = self.match_code_map(locations, loc_dict)
+        # job_list = self.match_code_map(jobs, job_dict)
 
-        self.__params['access_key'] = self.service_key
-        self.__params['loc_cd'] = ','.join(loc_list)
-        self.__params['job_cd'] = ','.join(job_list)
-        self.__params['fields'] = self.__fields
-        self.__params['sr'] = 'directhire'
-        self.__params['count'] = requires['count']
-    
-    def map_locations(self, locs: list, lmap: dict) -> list:
-        loc_keys = lmap.keys()
-        return[ lmap[loc] if loc in loc_keys else '' for loc in locs ]
+        fields = {'posting-date', 'expiration-date', 'keyword-code', 'count'}
+
+        self.params['access-key'] = self.service_key
+        # self.params['loc-cd'] = ','.join(loc_list)
+        # self.params['job-cd'] = ','.join(job_list)
+        self.params['loc-cd'] = '101000'
+        self.params['job-cd'] = '181'
+        self.params['fields'] = fields
+        self.params['sr'] = 'directhire'
+        self.params['count'] = requires['count']
+
+
+    def match_code_map(self, texts: list, codes: dict) -> list:
+        code_keys = codes.keys()
+        return [ codes[text] if text in code_keys else '' for text in texts ]
+
 
     def map_jobs(self, jobs: list, jmap: dict) -> list:
         job_keys = jmap.keys()
-        return[ jmap[job] if job in job_keys else '' for job in jobs ]
+        return [ jmap[job] if job in job_keys else '' for job in jobs ]
+
+
+    def request_json(self):
+        response = requests.get(url=self.service_url, \
+                                headers=self.headers, \
+                                params=self.params)
+
+        self.service_response = json.loads(response.content)
+
+
+    def make_content(self):
+        if not self.service_response:
+            raise Exception('서버로부터 응답을 받지 못했습니다.')
+
+        recruits = self.service_response['jobs']['job']
+
+        for recruit in recruits:
+            recruit_info = dict()
+            company = recruit['company']['detail']
+            position = recruit['position']
+
+            opening_date = int(recruit['opening-timestamp'])
+            opening_date = date.fromtimestamp(opening_date)
+
+            expiration_date = int(recruit['expiration-timestamp'])
+            expiration_date = date.fromtimestamp(expiration_date)
+
+            recruit_info['href'] = company['href']
+            recruit_info['id'] = recruit['id']
+            recruit_info['title'] = position['title']
+            recruit_info['industry'] = position['industry']['name']
+            recruit_info['location'] = position['location']['name']
+            recruit_info['job_type'] = position['job-type']['name']
+            recruit_info['exp'] = position['experience-level']['name']
+            recruit_info['edu'] = position['required-education-level']['name']
+            recruit_info['keyword'] = recruit['keyword']
+            recruit_info['salary'] = recruit['salary']
+            recruit_info['opening_date'] = opening_date
+            recruit_info['expiration_date'] = expiration_date
+            recruit_info['dday'] = (expiration_date - date.today()).days
+            recruit_info['close-type'] = recruit['close-type']['name']
+
+            self.content[company['name']] = recruit_info
+
+"""
+requires {
+    'locations': 희망 지역 리스트
+    'jobs': 희망 직업 리스트
+    'assay': 자기소개서 포함 여부 (True/False) -> 잡코리아 연결
+    'skill': 기술 역량 포함 여부 (True/False) -> 원티드 연결
+    'skill_map': 조건에 맞는 전체 기술 역량의 워드 클라우드 포함 여부 (True/False) -> 원티드 연결
+    'count': 보여줄 채용공고 개수 (기본값: 5, 최대값: 110)
+    'code_map': 지역, 직업 파라미터 코드 딕셔너리
+    ('companies': 클라이언트가 지정한 특정 회사 목록,
+                  없을 경우 사람인 API에서 가져온 회사명 목록으로 대체,
+                  잡코리아 및 원티드에서 사용)
+}
+"""
